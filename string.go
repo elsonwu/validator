10f6102944e2c13021c9bd0b9f37aaa200fb1a96
elsonwu <elsonwu@126.com>
2014-06-20 11:49:51 +0800

    use is package

--- string.go
+++ string.go
@@ -3,13 +3,10 @@ package validator
 import (
 	"errors"
 	"reflect"
-	"regexp"
 	"strconv"
-)
 
-const (
-	EmailPattern = `^[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$`
-	UrlPattern   = `((http|https|ftp){1}\:\/\/)?([a-zA-Z0-9\-\.]+[a-zA-Z0-9]+\.(net|cn|co|hk|tw|com|edu|gov|us|int|mil|org|int|mil|vg|uk|idv|tk|se|nz|nu|nl|ms|jp|jobs|it|ind|gen|firm|in|gs|fr|fm|eu|es|de|bz|be|at|am|ag|mx|asia|ws|xxx|tv|cc|ca|mobi|me|biz|arpa|info|name|pro|aero|coop|museum|ly|eg|mk)(:[a-zA-Z0-9]*)?\/?([a-zA-Z0-9\-\._\?\'\/\\\+&amp;%\$#\=~])*)+`
+	"github.com/elsonwu/i18n"
+	"github.com/elsonwu/is"
 )
 
 type String struct {
@@ -22,7 +19,7 @@ func (self *String) Filter(f reflect.StructField, fv reflect.Value) bool {
 
 func (self *String) Validate(f reflect.StructField, fv reflect.Value) (errs []error) {
 	if "required" == f.Tag.Get("required") && "" == fv.String() {
-		errs = append(errs, errors.New(f.Name+" cannot be blank"))
+		errs = append(errs, errors.New(i18n.T("%s cannot be blank", f.Name)))
 	}
 
 	min := f.Tag.Get("min")
@@ -33,7 +30,7 @@ func (self *String) Validate(f reflect.StructField, fv reflect.Value) (errs []er
 		}
 
 		if min2 > fv.Len() {
-			errs = append(errs, errors.New(f.Name+": min err"))
+			errs = append(errs, errors.New(i18n.T("%s min err", f.Name)))
 		}
 	}
 
@@ -45,24 +42,20 @@ func (self *String) Validate(f reflect.StructField, fv reflect.Value) (errs []er
 		}
 
 		if max2 < fv.Len() {
-			errs = append(errs, errors.New(f.Name+": max err"))
+			errs = append(errs, errors.New(i18n.T("%s max err", f.Name)))
 		}
 	}
 
-	is := f.Tag.Get("is")
-	if "" != is {
-		switch is {
+	isv := f.Tag.Get("is")
+	if "" != isv {
+		switch isv {
 		case "email":
-			if ok, err := regexp.MatchString(EmailPattern, fv.String()); !ok {
-				errs = append(errs, errors.New(f.Name+": is not a valid email"))
-			} else if err != nil {
-				errs = append(errs, err)
+			if !is.Email(fv.String()) {
+				errs = append(errs, errors.New(i18n.T("%s is not a valid email", f.Name)))
 			}
 		case "url":
-			if ok, err := regexp.MatchString(UrlPattern, fv.String()); !ok {
-				errs = append(errs, errors.New(f.Name+": is not a valid url"))
-			} else if err != nil {
-				errs = append(errs, err)
+			if !is.Url(fv.String()) {
+				errs = append(errs, errors.New(i18n.T("%s is not a valid url", f.Name)))
 			}
 		}
 	}
